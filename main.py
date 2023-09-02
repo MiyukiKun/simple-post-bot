b2f2e46d2e7cd6efb4ecfbf7fb232e2540a304ae
Majethia <hardikmajethia9529@gmail.com>
2023-09-02 12:40:47 +0530

    many buttons

--- main.py
+++ main.py
@@ -8,16 +8,31 @@ async def _(event):
         await conv.send_message('Message Text: ')
         txt = await conv.get_response()
         txt = txt.raw_text
-        await conv.send_message('Button Link: ')
-        link = await conv.get_response()
-        link = link.raw_text
+        await conv.send_message('How many rows of buttons you want?')
+        r = await conv.get_response()
+        r = int(r.raw_text)
+        await conv.send_message('How many colunms of buttons you want?')
+        c = await conv.get_response()
+        c = int(c.raw_text)
+        buttons = []
+        for i in range(r):
+            temp = []
+            for j in range(c):
+                await conv.send_message(f'Button row:{i+1} col:{j+1} Text: ')
+                btxt = await conv.get_response()
+                btxt = btxt.raw_text
+                await conv.send_message(f'Button row:{i+1} col:{j+1} Link: ')
+                link = await conv.get_response()
+                link = link.raw_text
+                temp.append(Button.url(text=btxt, url=link))
+        buttons.append(temp)
         await conv.send_message('Target Channel Id: ')
         cid = await conv.get_response()
         cid = cid.raw_text
         if not cid.startswith("-100"):
             cid = f"-100{cid}"
 
-        msg = await bot.send_message(int(cid), f"`{txt}`", buttons=[Button.url(text="Click here to download", url=link)])
+        msg = await bot.send_message(int(cid), f"`{txt}`", buttons=buttons)
         await event.reply(f"Posted to channel\n\n t.me/c/{cid.replace('-100', '')}/{msg.id}")
 
 bot.start()
